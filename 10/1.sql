e103018875033092215c31c9c6e932c67b5ef477
Thomas Brinkman <tj.brinkman@gmail.com>
2021-12-10 15:18:17 +0100

    oops... table --> view

--- 10/1.sql
+++ 10/1.sql
@@ -1,4 +1,4 @@
-create or replace table day_10_chunks as 
+create or replace view day_10_chunks as 
 	select	d.id,
 			s.seq as ch, 
 			substr(d.chunks, s.seq, 1) as tag
@@ -6,14 +6,14 @@ create or replace table day_10_chunks as
 			join seq_1_to_255 s on 1=1
 	where	s.seq <= length(d.chunks);
 
-create or replace table day_10_tags as 
+create or replace view day_10_tags as 
 		select '(' as opening, ')' as closing, 3 as points
 		union select '[',']', 57
 		union select '{','}', 1197
 		union select '<','>', 25137;		
 
 with
-	recursive cte_corrupt as (
+	recursive cte_stack as (
 		-- recursion entry point
 		select	c.id,
 				c.ch,
@@ -26,28 +26,28 @@ with
 		select	c.id,
 				c.ch,
 				c.tag,
-				case -- stack management....
+				case 
 					when c.tag in (select opening from day_10_tags)
-					then concat(cte.stack, c.tag) -- new opening, push to stack
+					then concat(cte.stack, c.tag) -- new opening
 					else -- closing, correct?
 						case
 							when c.tag = (select closing from day_10_tags where opening = right(cte.stack,1))
-							then substr(cte.stack, 1, length(cte.stack)-1) -- correct, pop from stack
-							else concat((select closing from day_10_tags where opening = right(cte.stack,1)), ' EXPECTED') -- err
+							then substr(cte.stack, 1, length(cte.stack)-1) -- correct
+							else concat((select closing from day_10_tags where opening = right(cte.stack,1)), ' EXPECTED')
 						end
 				end as stack
-		from 	cte_corrupt cte 
+		from 	cte_stack cte 
 				join day_10_chunks c
 				  on c.id = cte.id 
 				 and c.ch = cte.ch + 1
 	),
 	cte_first_error as (
 		select	id, min(ch) as first_err
-		from 	cte_corrupt	  
+		from 	cte_stack	  
 		where	instr(stack, 'EXPECTED') > 1
 		group by id
 	)
 select	sum(points)
-from 	cte_corrupt corrupt
+from 	cte_stack corrupt
 		join day_10_tags tags on tags.closing = corrupt.tag
 where	(id, ch) in ( select id, first_err from cte_first_error )	
